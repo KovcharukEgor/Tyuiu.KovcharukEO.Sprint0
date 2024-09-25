e76c110020c889490a39cf14c711ef98f00af2e1
KovcharukEgor <1hatsu1337@gmail.com>
2024-09-25 11:15:51 +0500

    Task5

---
+++ Tyuiu.KovcharukEO.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KovcharukEO.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.KovcharukEO.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(5, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 7));
+            Console.WriteLine("A * B = " + DataService.Multiplication(15, 10));
+            Console.WriteLine("a / b = " + DataService.Division(9, 9));
+            Console.ReadKey();
+        }
+    }
+}
